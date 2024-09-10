4d8bbaecde5e9e63d1a77160d5115e8032d54855
maryam-deriv <maryam@regentmarkets.com>
2024-09-10 10:37:45 +0000

    update

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+# Simple Dockerfile
+FROM alpine:latest
+
+# Set default command
+CMD ["echo", "Hello from my Docker image!"]
+
---
+++ build_docker_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables
+IMAGE_NAME="my-simple-image"
+IMAGE_TAG="1"
+
+# Build the Docker image
+echo "Building Docker image: $IMAGE_NAME:$IMAGE_TAG"
+docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  echo "Docker image $IMAGE_NAME:$IMAGE_TAG built successfully!"
+else
+  echo "Failed to build Docker image."
+  exit 1
+fi
