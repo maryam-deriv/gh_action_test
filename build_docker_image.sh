dbfa9f699febd6a0e1b609147d76228b14757e57
maryam-deriv <maryam@regentmarkets.com>
2024-09-11 12:22:59 +0000

    added base image

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Variables
-IMAGE_NAME="maryamdrv/my-simple-image"
-IMAGE_TAG="1.6"
+# Default values
+BASE_IMAGE_NAME="maryamdrv/base-image"
+BASE_IMAGE_TAG="1.0"
+
+SERVICE_IMAGE_NAME="maryamdrv/my-simple-image"
+SERVICE_IMAGE_TAG="1.7"
+
+DOCKERFILE="Dockerfile"
+
+# Check the argument and set Dockerfile and image details accordingly
+if [ "$1" == "build_base_image" ]; then
+  IMAGE_NAME="$BASE_IMAGE_NAME"
+  IMAGE_TAG="$BASE_IMAGE_TAG"
+  DOCKERFILE="base_image/Dockerfile"
+elif [ "$1" == "build_service_image" ]; then
+  IMAGE_NAME="$SERVICE_IMAGE_NAME"
+  IMAGE_TAG="$SERVICE_IMAGE_TAG"
+  DOCKERFILE="Dockerfile"
+else
+  echo "Invalid argument. Use 'build_base_image' or 'build_service_image'."
+  exit 1
+fi
 
 # Build the Docker image
-echo "Building Docker image: $IMAGE_NAME:$IMAGE_TAG"
-docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+echo "Building Docker image: $IMAGE_NAME:$IMAGE_TAG with Dockerfile: $DOCKERFILE"
+docker build -t "$IMAGE_NAME:$IMAGE_TAG" -f "$DOCKERFILE" .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
