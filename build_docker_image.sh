1beb86c4f379fecdeadb30b6a1dc200bf360da03
maryam-deriv <maryam@regentmarkets.com>
2024-09-13 04:07:52 +0000

    test: update version service image, new digest should be added to base file

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -5,7 +5,7 @@ BASE_IMAGE_NAME="maryamdrv/base-image"
 BASE_IMAGE_TAG="1.5"
 
 SERVICE_IMAGE_NAME="maryamdrv/my-simple-image"
-SERVICE_IMAGE_TAG="1.15"
+SERVICE_IMAGE_TAG="1.16"
 
 DOCKERFILE="Dockerfile"
 
