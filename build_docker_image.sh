8a8ff208cac8dcfffd867f8b15231c47ca39d34c
maryam-deriv <maryam@regentmarkets.com>
2024-09-18 09:09:25 +0000

    case: base_image should be build

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -2,10 +2,10 @@
 
 # Default values
 BASE_IMAGE_NAME="maryamdrv/base-image"
-BASE_IMAGE_TAG="1.5"
+BASE_IMAGE_TAG="1.6"
 
 SERVICE_IMAGE_NAME="maryamdrv/my-simple-image"
-SERVICE_IMAGE_TAG="1.19"
+SERVICE_IMAGE_TAG="1.20"
 
 DOCKERFILE="Dockerfile"
 
