f5b2b47038e2a453c58c6851b8b05c64c907bf54
maryam-deriv <maryam@regentmarkets.com>
2024-09-23 05:29:44 +0000

    trigger

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -5,7 +5,7 @@ BASE_IMAGE_NAME="maryamdrv/base-image"
 BASE_IMAGE_TAG="1.7"
 
 SERVICE_IMAGE_NAME="maryamdrv/my-simple-image"
-SERVICE_IMAGE_TAG="1.22"
+SERVICE_IMAGE_TAG="1.23"
 
 DOCKERFILE="Dockerfile"
 
